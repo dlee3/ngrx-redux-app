ab87192df8c3596ed743f0d06de4ef3bb167f487
David Lee <dhlee2@gmail.com>
2018-03-07 16:14:11 -0600

    feat(course): add update, remove pizza

--- src/products/store/actions/pizzas.action.ts
+++ src/products/store/actions/pizzas.action.ts
@@ -12,13 +12,11 @@ export class LoadPizzas implements Action {
 
 export class LoadPizzasFail implements Action {
   readonly type = LOAD_PIZZAS_FAIL;
-
   constructor(public payload: any) { }
 }
 
 export class LoadPizzasSuccess implements Action {
   readonly type = LOAD_PIZZAS_SUCCESS;
-
   constructor(public payload: Pizza[]) {}
 }
 
@@ -42,6 +40,46 @@ export class CreatePizzaSuccess implements Action {
   constructor(public payload: Pizza) {}
 }
 
+// update pizza
+export const UPDATE_PIZZA = '[Products] Update Pizza';
+export const UPDATE_PIZZA_FAIL = '[Products] Update Pizza Fail';
+export const UPDATE_PIZZA_SUCCESS = '[Products] Update Pizza Success';
+
+export class UpdatePizza implements Action {
+  readonly type = UPDATE_PIZZA;
+  constructor(public payload: Pizza) {}
+}
+
+export class UpdatePizzaFail implements Action {
+  readonly type = UPDATE_PIZZA_FAIL;
+  constructor(public payload: any) {}
+}
+
+export class UpdatePizzaSuccess implements Action {
+  readonly type = UPDATE_PIZZA_SUCCESS;
+  constructor(public payload: Pizza) {}
+}
+
+// delete pizza
+export const REMOVE_PIZZA = '[Products] Delete Pizza';
+export const REMOVE_PIZZA_FAIL = '[Products] Delete Pizza Fail';
+export const REMOVE_PIZZA_SUCCESS = '[Products] Delete Pizza Success';
+
+export class RemovePizza implements Action {
+  readonly type = REMOVE_PIZZA;
+  constructor(public payload: Pizza) {}
+}
+
+export class RemovePizzaFail implements Action {
+  readonly type = REMOVE_PIZZA_FAIL;
+  constructor(public payload: any) {}
+}
+
+export class RemovePizzaSuccess implements Action {
+  readonly type = REMOVE_PIZZA_SUCCESS;
+  constructor(public payload: Pizza) {}
+}
+
 // pizza action types
 export type PizzasAction =
   LoadPizzas |
@@ -49,4 +87,10 @@ export type PizzasAction =
   LoadPizzasSuccess |
   CreatePizza |
   CreatePizzaFail |
-  CreatePizzaSuccess;
+  CreatePizzaSuccess |
+  UpdatePizza |
+  UpdatePizzaFail |
+  UpdatePizzaSuccess |
+  RemovePizza |
+  RemovePizzaFail |
+  RemovePizzaSuccess;
